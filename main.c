7641c78a43cef3518717544f16eba6042c60894b
Globson <samuelpedro1999@gmail.com>
2020-11-02 23:27:47 -0300

    Removi coments do main.

--- main.c
+++ main.c
@@ -4,28 +4,7 @@
 #include <time.h>
 int main(int argc, char const *argv[]) {
   srand(time(NULL));
-  // Testando entrada por arquivo e por teclado.
-  /*FILE *ArquivoControle;
-  char ch[2], str_enviada[100];
-  ArquivoControle = fopen("./Arquivos_Entrada/Controle.txt","r");
-  while(fscanf(ArquivoControle,"%s",ch) != EOF){
-    strcat(str_enviada,ch);
-    strcat(str_enviada," ");
-}
-  printf("\t%s\n",str_enviada);
-
-  char comando,str_enviada[100];;
-  int i = 0;
-  do{
-      scanf(" %c",&comando);
-      str_enviada[i] = comando;
-      str_enviada[i+1]= ' ';
-      i+=2;
-
-    }while(comando != 'M');
-    printf("\t%s\n",str_enviada);
-*/
-  Executar_P_Controle(); //Comecei os testes mesmo estado incompleto
+  Executar_P_Controle();
   printf("\n\tExecutou TUDO!");
   return 0;
 }
